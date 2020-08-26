2db9b9d8bb4ac135dbb63c3a10aa37ee1148abaa
alireza <ojworldreactjs@gmail.com>
2020-08-27 00:03:49 +0430

    check route is working

--- server.js
+++ server.js
@@ -5,7 +5,37 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
 const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV;
+
 const app = express();
+/*routes for bootcamps*/
+//get all bootcamps
+app.get("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "Show all bootcamps" });
+});
+//get single bootcamp
+app.get("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+});
+//add bootcamp
+app.post("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "Create a new bootcamp" });
+});
+
+//update bootcamp
+app.put("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+});
+
+//detele bootcamp
+app.delete("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+});
 
 app.listen(
   PORT,
